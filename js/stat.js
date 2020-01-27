3828d84d85ef2fa51776da84934b41213e0f51cd
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-01-27 19:08:36 +0300

    Cloud variables added

--- js/stat.js
+++ js/stat.js
@@ -2,13 +2,16 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_GAP = 10;
 
-var renderCloud = function(ctx, x, y, color) {
-  ctx.fillStyle = 'color';
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(100, 10, 'white');
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
 };
