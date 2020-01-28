25801605738494f27a6207159c23f8abc9ec521b
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-01-28 18:13:54 +0300

    Started removing unused variables

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,8 @@ var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var MAX_BAR_HEIGHT = 150;
 var BAR_TEXT = CLOUD_Y + 250;
+var BAR_TEXT_GAP = 20;
+var BAR_TIME_GAP = 10;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -25,35 +27,35 @@ window.renderStatistics = function (ctx) {
 
   var playerIndex = 0;
   var playerName = 'Вы';
-  var playerScore = '2725';
+  var playerTime = 1725;
   var barX = CLOUD_X + 50;
-  var barHeight = MAX_BAR_HEIGHT / (4025 / playerScore);
-  var barY = BAR_TEXT - 20 - barHeight;
-  var barScore = barY - 10;
+  var barHeight = MAX_BAR_HEIGHT / (4025 / playerTime);
+  var barY = BAR_TEXT - BAR_TEXT_GAP - barHeight;
+  var barScore = BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (4025 / playerTime) - BAR_TIME_GAP;
 
   ctx.fillStyle = 'black';
   ctx.fillText(playerName, barX, BAR_TEXT);
-  ctx.fillText(playerScore, barX, barScore);
+  ctx.fillText(playerTime, barX, barScore);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
+  ctx.fillRect(barX + 0 * (BAR_GAP + BAR_WIDTH), BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (4025 / playerTime), BAR_WIDTH, MAX_BAR_HEIGHT / (4025 / playerTime));
 
   ctx.fillStyle = 'black';
   ctx.fillText(playerName, barX + BAR_GAP + BAR_WIDTH, BAR_TEXT);
-  ctx.fillText(playerScore, barX + BAR_GAP + BAR_WIDTH, barScore);
+  ctx.fillText(playerTime, barX + BAR_GAP + BAR_WIDTH, barScore);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(barX + BAR_GAP + BAR_WIDTH, barY, BAR_WIDTH, barHeight);
+  ctx.fillRect(barX + 1 * (BAR_GAP + BAR_WIDTH), BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (4025 / playerTime), BAR_WIDTH, MAX_BAR_HEIGHT / (4025 / playerTime));
 
   ctx.fillStyle = 'black';
   ctx.fillText(playerName, barX + 2 * (BAR_GAP + BAR_WIDTH), BAR_TEXT);
-  ctx.fillText(playerScore, barX + 2 * (BAR_GAP + BAR_WIDTH), barScore);
+  ctx.fillText(playerTime, barX + 2 * (BAR_GAP + BAR_WIDTH), barScore);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(barX + 2 *(BAR_GAP + BAR_WIDTH), barY, BAR_WIDTH, barHeight);
+  ctx.fillRect(barX + 2 * (BAR_GAP + BAR_WIDTH), BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (4025 / playerTime), BAR_WIDTH, MAX_BAR_HEIGHT / (4025 / playerTime));
 
   ctx.fillStyle = 'black';
   ctx.fillText(playerName, barX + 3 * (BAR_GAP + BAR_WIDTH), BAR_TEXT);
-  ctx.fillText(playerScore, barX + 3 * (BAR_GAP + BAR_WIDTH), barScore);
+  ctx.fillText(playerTime, barX + 3 * (BAR_GAP + BAR_WIDTH), barScore);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(barX + 3 * (BAR_GAP + BAR_WIDTH), barY, BAR_WIDTH, barHeight);
+  ctx.fillRect(barX + 3 * (BAR_GAP + BAR_WIDTH), BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (4025 / playerTime), BAR_WIDTH, MAX_BAR_HEIGHT / (4025 / playerTime));
 
 };
 
