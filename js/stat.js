564b3572e61424e6766f549216a2936b44bbae57
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-01-29 12:21:45 +0300

    ParseInt added

--- js/stat.js
+++ js/stat.js
@@ -12,8 +12,8 @@ var BAR_TEXT = CLOUD_Y + 250;
 var BAR_TEXT_GAP = 20;
 var BAR_TIME_GAP = 10;
 
-var times = [2725, 4025, 1244, 1339];
-var names = ['Вы', 'Кекс', 'Катя', 'Игорь'];
+// var times = [2725, 4025, 1244, 1339];
+// var names = ['Вы', 'Кекс', 'Катя', 'Игорь'];
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -31,7 +31,7 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
   ctx.fillStyle = 'black';
@@ -44,9 +44,9 @@ window.renderStatistics = function (ctx) {
   for (var i = 0; i < 4; i++) {
     ctx.fillStyle = 'black';
     ctx.fillText(names[i], CLOUD_X + BAR_GAP + i * (BAR_GAP + BAR_WIDTH), BAR_TEXT);
-    ctx.fillText(times[i], CLOUD_X + BAR_GAP + i * (BAR_GAP + BAR_WIDTH), (BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (maxTime / times[i])) - BAR_TIME_GAP);
+    ctx.fillText(parseInt(times[i]), CLOUD_X + BAR_GAP + i * (BAR_GAP + BAR_WIDTH), (BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (maxTime / times[i])) - BAR_TIME_GAP);
     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    ctx.fillRect(CLOUD_X + BAR_GAP + i * (BAR_GAP + BAR_WIDTH), BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (4025 / times[i]), BAR_WIDTH, MAX_BAR_HEIGHT / (maxTime / times[i]));
+    ctx.fillRect(CLOUD_X + BAR_GAP + i * (BAR_GAP + BAR_WIDTH), BAR_TEXT - BAR_TEXT_GAP - MAX_BAR_HEIGHT / (maxTime / times[i]), BAR_WIDTH, MAX_BAR_HEIGHT / (maxTime / times[i]));
   }
 };
 
