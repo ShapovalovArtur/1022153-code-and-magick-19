3b485ff882d1053ea5d6ca2986f571e9bb979513
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-02-04 16:53:17 +0300

    Space between names added

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@ var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnames = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 var wizardNames = [];
@@ -23,7 +23,7 @@ var getRandomIndex = function (arr) {
 
 var getRandomName = function (firstname, surname) {
   for (var i = 0; i < firstNames.length; i++) {
-    wizardNames[i] = firstname[getRandomIndex(firstNames)] + surname[getRandomIndex(firstNames)];
+    wizardNames[i] = firstname[getRandomIndex(firstNames)] + ' ' + surname[getRandomIndex(firstNames)];
   }
   return wizardNames;
 };
