349e4dec9780617a022f4c3e78091a2f28150966
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-02-04 12:49:24 +0300

    Wizards done

--- js/setup.js
+++ js/setup.js
@@ -7,8 +7,13 @@ var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кр
 var surnames = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-var MageNames = [];
-var mages = [];
+var wizardNames = [];
+var wizards = [];
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
 
 var getRandomIndex = function (arr) {
   return Math.floor(Math.random() * (arr.length - 1));
@@ -16,22 +21,37 @@ var getRandomIndex = function (arr) {
 
 var getRandomName = function (firstname, surname) {
   for (var i = 0; i < firstNames.length; i++) {
-    MageNames[i] = firstname[getRandomIndex(firstNames)] + surname[getRandomIndex(firstNames)];
+    wizardNames[i] = firstname[getRandomIndex(firstNames)] + surname[getRandomIndex(firstNames)];
   }
-  return MageNames;
+  return wizardNames;
 };
 
-var createMagesList = function () {
-  for (var i = 0; i < MageNames.length; i++) {
-    mages[i] = {
-      name: MageNames[i],
+var createWizardList = function () {
+  for (var i = 0; i < wizardNames.length; i++) {
+    wizards[i] = {
+      name: wizardNames[i],
       coatColor: coatColors[getRandomIndex(coatColors)],
       eyesColor: eyesColors[getRandomIndex(eyesColors)]
     };
   }
 };
 
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
 getRandomName(firstNames, surnames);
-createMagesList();
+createWizardList();
 
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
 
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
