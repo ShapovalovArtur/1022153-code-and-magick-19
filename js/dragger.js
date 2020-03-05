5ea2eaf51841f636b98ce935ce7294c6f70fbe97
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-03-05 14:12:52 +0300

    Dragndrop added

---
+++ js/dragger.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function () {
+
+  var setup = document.querySelector('.setup');
+  var setupHandle = setup.querySelector('.upload');
+
+  setupHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var dragged = false;
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var mouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY,
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y + 'px');
+      setup.style.left = (setup.offsetLeft - shift.x + 'px');
+    };
+
+    var mouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+
+      if (dragged) {
+        var preventDefaulClickHandler = function (clickEvt) {
+          clickEvt.preventDefault();
+          setupHandle.removeEventListener('click', preventDefaulClickHandler);
+        };
+        setupHandle.addEventListener('click', preventDefaulClickHandler);
+      }
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  });
+})();
