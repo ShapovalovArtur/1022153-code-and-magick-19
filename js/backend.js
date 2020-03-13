133cafae8baab391e919679906b53ff2c3f4aba7
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-03-13 17:18:49 +0300

    Handlers done

--- js/backend.js
+++ js/backend.js
@@ -2,33 +2,59 @@
 
 (function () {
 
-  window.save = function (data, successHandler) {
+  var save = function (data, successHandler, errorHandler) {
     var URL = 'https://js.dump.academy/code-and-magick';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      successHandler(xhr.response);
+      if (xhr.status === 200) {
+        successHandler(xhr.response);
+      } else {
+        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      errorHandler('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      errorHandler('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
+    xhr.timeout = 10000;
     xhr.open('POST', URL);
     xhr.send(data);
 
   };
 
-  window.load = function (successHandler, errorHandler) {
+  var load = function (successHandler, errorHandler) {
     var URL = 'https://js.dump.academy/code-and-magick/data';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
-    xhr.open('GET', URL);
-
     xhr.addEventListener('load', function () {
-      successHandler(xhr.response);
+      if (xhr.status === 200) {
+        successHandler(xhr.response);
+      } else {
+        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      errorHandler('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      errorHandler('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
+    xhr.timeout = 10000;
+    xhr.open('GET', URL);
     xhr.send();
 
+  };
+
+  window.backend = {
+    save: save,
+    load: load
   };
 
 })();
--- js/setup.js
+++ js/setup.js
@@ -18,7 +18,7 @@
     return wizardElement;
   };
 
-  window.load(function (wizards) {
+  var successLoadHandler = function (wizards) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < WIZARD_QUANTITY; i++) {
@@ -27,15 +27,30 @@
     similarListElement.appendChild(fragment);
 
     userDialog.querySelector('.setup-similar').classList.remove('hidden');
-  });
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successLoadHandler, errorHandler);
 
   var form = userDialog.querySelector('.setup-wizard-form');
 
-  form.addEventListener('submit', function (evt) {
-    window.save(new FormData(form), function (response) {
-      userDialog.classList.add('hidden');
-    });
-    evt.preventDefault();
-  });
+  var successSaveHandler = function (response) {
+    userDialog.classList.add('hidden');
+  };
 
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.backend.save(new FormData(form), successSaveHandler, errorHandler);
+  });
 })();
