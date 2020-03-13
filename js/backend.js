6eb4d32fe724719ef57bfb926d0ddae4d834ae06
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-03-13 12:42:50 +0300

    Backend iife

--- js/backend.js
+++ js/backend.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var URL = 'https://js.dump.academy/code-and-magick';
+(function () {
+  var URL = 'https://js.dump.academy/code-and-magick';
 
-window.save = function (data, successHandler) {
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
+  window.save = function (data, successHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
-  xhr.addEventListener('load', function () {
-    successHandler(xhr.response);
-  });
+    xhr.addEventListener('load', function () {
+      successHandler(xhr.response);
+    });
 
-  xhr.open('POST', URL);
-  xhr.send(data);
+    xhr.open('POST', URL);
+    xhr.send(data);
 
-};
+  };
+})();
