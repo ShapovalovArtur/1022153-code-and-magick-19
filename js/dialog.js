69ed42428ceba29f5c5b545e2a22d292ea8dc670
ShapovalovArtur <ShapovalovArtur@users.noreply.github.com>
2020-03-18 12:35:08 +0300

    Wizard Color customizing moved to setup

--- js/dialog.js
+++ js/dialog.js
@@ -2,31 +2,6 @@
 
 (function () {
 
-  var COATS = [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ];
-
-  var EYES = [
-    'black',
-    'red',
-    'blue',
-    'yellow',
-    'green'
-  ];
-
-  var FIREBALLS = [
-    '#ee4830',
-    '#30a8ee',
-    '#5ce6c0',
-    '#e848d5',
-    '#e6e848'
-  ];
-
   var ESC_KEY = 'Escape';
   var ENTER_KEY = 'Enter';
 
@@ -34,12 +9,7 @@
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var userNameInput = setup.querySelector('.setup-user-name');
-  var coat = document.querySelector('.wizard-coat');
-  var eyes = document.querySelector('.wizard-eyes');
-  var fireball = document.querySelector('.setup-fireball-wrap');
-  var coatColorInput = document.querySelector('input[name = coat-color]');
-  var eyeColorInput = document.querySelector('input[name = eyes-color]');
-  var fireballColorInput = document.querySelector('input[name = fireball-color]');
+
 
   var popupEscHandler = function (evt) {
     if (userNameInput === document.activeElement) {
@@ -50,10 +20,6 @@
     }
   };
 
-  var colorChangeHandler = function (arr) {
-    return arr[window.util.getRandomIndex(arr)];
-  };
-
   var openPopup = function () {
     setup.classList.remove('hidden');
     document.addEventListener('keydown', popupEscHandler);
@@ -62,17 +28,6 @@
   var closePopup = function () {
     setup.classList.add('hidden');
     document.removeEventListener('keydown', popupEscHandler);
-  };
-
-  var changeColor = function (part, parts, partValue) {
-    part.style.fill = colorChangeHandler(parts);
-    partValue.value = part.style.fill;
-  };
-
-  var changeFireballColor = function () {
-    var currentColor = colorChangeHandler(FIREBALLS);
-    fireball.style.background = currentColor;
-    fireballColorInput.value = currentColor;
   };
 
   setupOpen.addEventListener('click', openPopup);
@@ -90,16 +45,4 @@
       openPopup();
     }
   });
-
-  coat.addEventListener('click', function () {
-    changeColor(coat, COATS, coatColorInput);
-  });
-
-
-  eyes.addEventListener('click', function () {
-    changeColor(eyes, EYES, eyeColorInput);
-  });
-
-
-  fireball.addEventListener('click', changeFireballColor);
 })();
